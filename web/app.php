3d1d315d679e627c0289194cc939fdcce0cfb733
Paul Chubatyy <xobb@citylance.biz>
2016-01-27 14:26:33 +0200

    Add installation instructions.
    Add symfony 2.8.2 bundles and everything.

---
+++ web/app.php
@@ -0,0 +1,42 @@
+<?php
+
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+// Enable APC for autoloading to improve performance.
+// You should change the ApcClassLoader first argument to a unique prefix
+// in order to prevent cache key conflicts with other applications
+// also using APC.
+/*
+$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+$loader->unregister();
+$apcLoader->register(true);
+*/
+
+$debug = false;
+// get the environment from the virtual host
+$environment = getenv('SYMFONY_ENV');
+if (is_null($environment))
+    $environment = 'prod';
+
+if ($environment == 'dev') {
+    $debug = true;
+    Debug::enable();
+}
+
+require_once __DIR__.'/../app/AppKernel.php';
+//require_once __DIR__.'/../app/AppCache.php';
+
+$kernel = new AppKernel($environment, $debug);
+$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
