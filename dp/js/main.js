b83c409806d37b7333808802b0b878b630d800b7
Карина <karina@MacBook-Pro-Karina.local>
2016-02-07 16:29:58 +0200

    time

--- dp/js/main.js
+++ dp/js/main.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 	$(function() {
 	    $( "#slider-range" ).slider({
 	      range: true,
-	      min: 0,
-	      max: 500,
-	      values: [ 75, 300 ],
+	      min: 1,
+	      max: 31,
+	      values: [ 1, 11 ],
 	      slide: function( event, ui ) {
-	        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+	        $( "#amount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
 	      }
 	    });
-	    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-	      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+	    $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
+	      " - " + $( "#slider-range" ).slider( "values", 1 ) );
   });
 });
